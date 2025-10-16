d477e2ed75043b808f75cca43e4ba03691f948f2
Josh <joshua.guto-21@cpu.edu.ph>
2025-10-16 00:32:30 -0400

    Arroyo

---
+++ lib/marketplace.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class marketplacepage extends StatefulWidget {
+  const marketplacepage({super.key});
+
+  @override
+  State<marketplacepage> createState() => _marketplacepageState();
+}
+
+class _marketplacepageState extends State<marketplacepage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/custommain.dart
@@ -0,0 +1,56 @@
+import 'package:facebook/pages/friends.dart';
+import 'package:facebook/pages/marketplace.dart';
+import 'package:facebook/pages/newsfeed.dart';
+import 'package:facebook/pages/notification.dart';
+import 'package:facebook/pages/profile.dart';
+import 'package:facebook/pages/reels.dart';
+import 'package:flutter/material.dart';
+
+class CustomMain extends StatefulWidget {
+  const CustomMain({super.key});
+
+  @override
+  State<CustomMain> createState() => _CustomMainState();
+}
+
+class _CustomMainState extends State<CustomMain> {
+  int currenPage = 0;
+
+  List pages = [
+    const NewsFeed(),
+    const reelspage(),
+    const friendspage(),
+    const marketplacepage(),
+    const notificationpage(),
+    const profilepage(),
+  ];
+
+void onTapped(int index){
+  setState(() {
+    currenPage = index;
+  });
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currenPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currenPage,
+        type: BottomNavigationBarType.fixed,
+        onTap: onTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.ondemand_video), label: "Reels"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Friends"),
+          BottomNavigationBarItem(icon: Icon(Icons.local_convenience_store), label: "Marketplace"),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: "Notifications"),
+          BottomNavigationBarItem(icon: CircleAvatar(
+            backgroundImage: AssetImage("assets/profile/prof1.jpg"),
+          ), label: "Profile"),
+        ]
+      ),
+    );
+  }
+}
